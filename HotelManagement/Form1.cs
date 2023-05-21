959b2eb0dd3b8c4c138fa29e0138a886d9ef1561
Chandra Prakash S <scp.thedevil@gmail.com>
2023-05-22 00:56:11 +0530

    Add files via upload

--- HotelManagement/Form1.cs
+++ HotelManagement/Form1.cs
@@ -2,9 +2,23 @@ namespace HotelManagement
 {
     public partial class Form1 : Form
     {
+        private Dictionary<string, string> validUsers;
+
         public Form1()
         {
             InitializeComponent();
+            InitializeValidUsers();
+        }
+
+        private void InitializeValidUsers()
+        {
+            validUsers = new Dictionary<string, string>
+            {
+                { "admin", "pass" },
+                { "user1", "1234" },
+                {"shakthi@intellect.com", "sha@20"  }
+                // Add more valid usernames and passwords as needed
+            };
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -24,23 +38,20 @@ namespace HotelManagement
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (txtUsername.Text == "admin" && txtPassword.Text == "pass")
+            if (AuthenticateUser(txtUsername.Text, txtPassword.Text))
             {
                 labelError.Visible = false;
                 Dashboard ds = new Dashboard();
                 this.Hide();
                 ds.Show();
-
-
             }
             else
             {
                 labelError.Visible = true;
                 txtPassword.Clear();
-
             }
         }
-         
+
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
@@ -49,6 +60,16 @@ namespace HotelManagement
         private void panel1_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private bool AuthenticateUser(string username, string password)
+        {
+            if (validUsers.ContainsKey(username))
+            {
+                string storedPassword = validUsers[username];
+                return password == storedPassword;
+            }
+            return false;
         }
     }
 }
