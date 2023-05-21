9ef668da5923ce34a3fa6ef95541b75c67b59902
Chandra Prakash S <scp.thedevil@gmail.com>
2023-05-21 13:36:14 +0530

    Add files via upload

--- HotelManagement/Dashboard.Designer.cs
+++ HotelManagement/Dashboard.Designer.cs
@@ -28,12 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(69, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(814, 126);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(69, 164);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(814, 403);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(21, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 66);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Add Room";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(185, 35);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 66);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Customer Reg";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(340, 35);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(145, 66);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Check Out";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(491, 35);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(154, 66);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Customer Details";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(651, 35);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(154, 66);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Employee";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(952, 579);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Dashboard";
             this.Text = "Dashboard";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Panel panel1;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Panel panel2;
     }
 }
